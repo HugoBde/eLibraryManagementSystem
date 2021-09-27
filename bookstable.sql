5b5b91cebb3de25789fe4c92b61e24e21c72bf86
Sheereen Sidiq Ali <79573748+SheereenSA@users.noreply.github.com>
2021-09-27 16:50:18 +1000

    Renew Book table

--- bookstable.sql
+++ bookstable.sql
@@ -6,3 +6,17 @@ CREATE TABLE table_name (
     DueDate DATE,
     DueTime Time(fsp),
 );
+
+<form name="form1" method="post" action="" onSubmit="return validate();">
+<table border="1" cellpadding="3" cellspacing="1" align="center">
+<tr>
+</tr>
+<tr>
+<td></td>
+<td><strong>Book Title</strong></td>
+<td><strong>BorrowedDate</strong></td>
+<td><strong>Duration</strong></td>
+<td><strong>Overdue</strong></td>
+<td><strong>Renewed</strong></td>
+<td><strong>Student ID</strong></td>
+</tr>
