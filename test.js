6578e3f36c39a1464e44dd67985285a64b14127c
Hugo Bde <bouderliqueh@gmail.com>
2021-10-12 16:16:03 +1100

    Improved exceptions handling

--- test.js
+++ test.js
@@ -31,14 +31,18 @@ async function test() {
             }
         })
         .catch(err => {
-            console.error(err.message)
+            console.error("\033[31m" + err.message + "\033[m")
         })
         testOutcomes.push(res)
 
     }
     Promise.allSettled(testOutcomes)
         .then(results => {
-            console.error("\n" + tests.length + " tests completed\n\033[32m" + successfulTests + " test(s) passed\n\033[31m" + unsuccessfulTests + " test(s) failed\033[0m\n")
+            completedTests = successfulTests + unsuccessfulTests
+            console.error("\n" + completedTests + " tests completed\n\033[32m" + successfulTests + " test(s) passed\n\033[31m" + unsuccessfulTests + " test(s) failed\033[0m\n")
+            if (completedTests !== tests.length) {
+                console.error("\033[31mNumber of tests completed (" + completedTests + ") does not match the number of detected tests (" + tests.length + "), please check the log for errors\033[m")
+            }
             if (unsuccessfulTests !== 0) {
                 console.error("Failed tests:")
                 for (let i = 0; i < unsuccessfulTests; i++) {
@@ -60,9 +64,5 @@ const {app} = require("./server")
 
 routes.connectToDB().then(() => {
     let server = app.listen(process.env.PORT || 3000)
-    test().then(
-        () => {
-            //server.close(()=> process.exit(0))
-        }
-    )
+    test()
 })
